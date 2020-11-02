65f72c895a8a0838b8f7c999b97557c1fabab22e
xuhongxin <445339054@qq.com>
2020-11-03 01:16:48 +0800

    fix comment

--- hard/trapping-rain-water/main.go
+++ hard/trapping-rain-water/main.go
@@ -10,7 +10,7 @@ import "fmt"
 左右俩指针，左右同时向中间移动
 ----
 
-> 以认为如果一端有更高的条形块（例如右端），积水的高度依赖于当前方向的高度（从左到右）。当我们发现另一侧（右侧）的条形块高度不是最高的，我们则开始从相反的方向遍历（从右到左）。
+> 认为如果一端有更高的条形块（例如右端），积水的高度依赖于当前方向的高度（从左到右）。当我们发现另一侧（右侧）的条形块高度不是最高的，我们则开始从相反的方向遍历（从右到左）。
 作者：LeetCode
 链接：https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-leetcode/
 来源：力扣（LeetCode）
@@ -38,6 +38,8 @@ func trap(height []int) int {
             } else {
                 // 这个时候之所以可以存储水，右边呢？别忘了右边在这个循环里面一定是大于当前的，所以必然是可以存储水的
                 trap += leftMax-height[left]
+                // 证明 leftMax <= height[right]
+                // 当前 if 是只有 height[left] < height[right] 的时候才会进入，所以当前 if 产生的 leftMax 也一定是小于 height[right]
             }
             left++
         } else {
